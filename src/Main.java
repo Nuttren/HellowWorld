4308f27ac4481931ed1d76e8e418964275c1e0b5
nuttren <roman.karpencko@gmail.com>
2022-12-21 16:33:12 +0400

    Массивы

--- src/Main.java
+++ src/Main.java
@@ -4,99 +4,68 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1();
-        task2();
-        task3();
+        task2 ();
+        task3 ();
         task4();
-
 
     }
 
     private static void task4() {
-        int[] nums = {3, 2, 5};
-        for (int index = 0; index < nums.length; index++) {
-            if (nums[index] % 2 != 0 && index == nums.length - 1) {
-                System.out.print(nums[index] + 1 + " ");
-            } else if (nums[index] % 2 != 0 && index != nums.length - 1)
-                System.out.print(nums[index] + 1 + ",");
-               else { System.out.print(nums[index] + ",");
-            }
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i >= 0; i--) {
+                System.out.print(reverseFullName[i]);
         }
     }
 
-
-
     private static void task3() {
-        {
-            int[] nums = {1, 2, 3};
-            for (int index = 2; index <= nums.length; index = index - 1) {
-                if (index == nums.length - 3) {
-                    System.out.print(nums[index]);
-                    System.out.println();
-                    break;
-                }
-                {
-                    System.out.print(nums[index] + ",");
-                }
-            }
-        }
-
-        {
-            double[] nums2 = {1.57, 7.64, 9.986};
-            for (int index2 = 2; index2 < nums2.length; index2 = index2 - 1) {
-                if (index2 == nums2.length - 3) {
-                    System.out.print(nums2[index2]);
-                    System.out.println();
-                    break;
-                }
-                {
-                    System.out.print(nums2[index2] + ",");
-                }
-            }
+        int[] arr = generateRandomArray();
+        float sum = 0;
+        float sumAverage = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[1];
+           sumAverage = sum / arr.length;
         }
         {
-            int[] nums3 = new int[10];
-            nums3[0] = 5;
-            for (int index3 = 9; index3 <= nums3.length; index3 = index3 - 1) {
-                if (index3 == nums3.length - 10) {
-                    System.out.print(nums3[index3]);
-                    System.out.println();
-                    break;
-                }
-                {
-                    System.out.print(nums3[index3] + ",");
-                }
-            }
+            System.out.println("Средняя сумма за месяц составила " + sumAverage + " рублей");
         }
     }
 
     private static void task2() {
-        int[] nums = new int[]{1, 2, 3};
-            for (int index = 0; index <= nums.length; index = index + 1) {
-                if (index == nums.length - 1) {
-                    System.out.print(nums[index]);
-                    System.out.println();
-                    break;
-                }
-                {
-                    System.out.print(nums[index] + ",");
-                }
-            }
-            double[] nums2 = {1.57, 7.64, 9.986};
-            {
-                System.out.println(Arrays.toString(nums2));
-            }
-            int[] nums3 = new int[10];
-            nums3[0] = 5;
-            {
-                System.out.println(Arrays.toString(nums3));
+        int[] arr = generateRandomArray();
+        int sumMin = 200_000;
+        int sumMax = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > sumMax) {
+                sumMax = arr[i];
             }
         }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]< sumMin) {
+                sumMin = arr[i];
+            }
+
+        }
+        System.out.println("Минимальная сумма трат за день составила " + sumMin + " рублей. Максимальная сумма трат за день составила " + sumMax + " рублей");
+    }
 
 
         private static void task1 () {
-            int[] nums = new int[]{1, 2, 3};
-            double[] nums2 = {1.57, 7.64, 9.986};
-            int[] nums3 = new int[10];
-
+            int[] arr = generateRandomArray();
+            int sum = 0;
+            for (int element: arr) {
+                sum += arr[1];
+            }
+            {
+                System.out.println("Сумма за месяц составила " + sum + " рублей");
+            }
         }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
+}
